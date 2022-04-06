149736092acfe84751496900184a0bf8a662dcb1
ZorudaRinku <stronger6@gmail.com>
2022-04-05 18:10:52 -0600

    Switched to logging instead of printing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import glob
 import os
 import cv2
 from tqdm import tqdm
-
+import logging
 
 def attributes():
 	start_pos, end_pos, resize_factor, duration, codec, codec_file = [], [], 0, 0, "", ""
@@ -30,14 +30,14 @@ def attributes():
 			codec = "mp4v"
 
 	except ValueError:
-		print("Invalid input, must be formatted as 'x,y'")
+		logging.info("Invalid input, must be formatted as 'x,y'")
 		exit()
 	except IndexError:
-		print("Invalid input, must be formatted as 'x,y'")
+		logging.info("Invalid input, must be formatted as 'x,y'")
 		exit()
 
 	if start_pos[0] > end_pos[0] or start_pos[1] > end_pos[1]:
-		print("Ending Position must be greater than Starting Position")
+		logging.info("Ending Position must be greater than Starting Position")
 		exit()
 
 	return start_pos, end_pos, resize_factor, duration, codec, codec_file
@@ -50,8 +50,9 @@ def crop_image(image, start_pos, end_pos, index):
 		cv2.imwrite("./cropped/" + str(index) + ".png", img)
 
 
-def main():
-	# Get the attributes from the user
+def main():	# Get the attributes from the user
+	logging.basicConfig(format='[%(asctime)s]: %(message)s', datefmt='%H:%M:%S')
+
 	start_pos, end_pos, resize_factor, duration, codec, codec_file = attributes()
 
 	# Check that we have Cisco Openh264 DLL installed in the script directory
@@ -61,21 +62,21 @@ def main():
 			download = input("OpenH264 not found, would you like to automatically download it from https://github.com/cisco/openh264/releases/tag/v1.8.0 ? (Y/n): ")
 			if download.lower() != "y" and download.lower() != "":
 				# Give instructions on how to download OpenH264 DLL
-				print("Please download OpenH264 DLL from https://github.com/cisco/openh264/releases/tag/v1.8.0, unpack it, and place it in the same directory as this script")
+				logging.error("Please download OpenH264 DLL from https://github.com/cisco/openh264/releases/tag/v1.8.0, unpack it, and place it in the same directory as this script")
 				exit()
 
 			# Download OpenH264 DLL from GitHub and unpack it into working directory
 			import requests
 			import bz2
 
-			print(f"Downloading OpenH264 from https://github.com/cisco/openh264/releases/download/v1.8.0/{codec_file}.bz2...")
+			logging.info(f"Downloading OpenH264 from https://github.com/cisco/openh264/releases/download/v1.8.0/{codec_file}.bz2...")
 			r = requests.get(f"https://github.com/cisco/openh264/releases/download/v1.8.0/{codec_file}.bz2")
 			if r.status_code != 200:
-				print(r.raise_for_status())
+				logging.error(r.raise_for_status())
 				exit()
 			open(codec_file + ".bz2", 'wb').write(r.content)
 
-			print("Extracting...")
+			logging.info("Extracting...")
 			zip_data = bz2.BZ2File(codec_file + ".bz2").read()
 			open(codec_file, 'wb').write(zip_data)
 			os.remove(codec_file + ".bz2")
@@ -87,10 +88,10 @@ def main():
 		os.makedirs("./cropped")
 
 	if len(os.listdir("./images")) == 0:
-		print("No images found, please place images in ./images")
+		logging.info("No images found, please place images in ./images")
 		download = input("Would you like to automatically download and unpack images from https://zevs.me/rplace_archive.7z (10.6GB)? (Y/n): ")
 		if download.lower() != "y" and download.lower() != "":
-			print("Please download images from https://zevs.me/rplace_archive.7z (Or elsewhere), and unpack them in ./images directory\n(Note: zevs.me archive has a subdirectory. Do not include this subdirectory in ./images)")
+			logging.error("Please download images from https://zevs.me/rplace_archive.7z (Or elsewhere), and unpack them in ./images directory\n(Note: zevs.me archive has a subdirectory. Do not include this subdirectory in ./images)")
 			exit()
 
 		# Download images from zevs.me and unpack them to ./images
@@ -100,15 +101,15 @@ def main():
 
 		# Download archive
 		if not os.path.exists("./rplace_archive.7z"):
-			print("Downloading images from https://zevs.me/rplace_archive.7z... This may take awhile...")
+			logging.info("Downloading images from https://zevs.me/rplace_archive.7z... This may take awhile...")
 			r = requests.get("https://zevs.me/rplace_archive.7z")
 			if r.status_code != 200:
-				print(r.raise_for_status())
+				logging.error(r.raise_for_status())
 				exit()
 			open("./rplace_archive.7z", 'wb').write(r.content)
 
 		# Unpack archive
-		print("Extracting... This may take awhile...")
+		logging.info("Extracting... This may take awhile...")
 		shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
 		shutil.unpack_archive('./rplace_archive.7z', './images')
 
@@ -140,11 +141,11 @@ def main():
 	out.release()
 
 	# Delete the cropped images from ./cropped
-	print("Done! Cleaning up files...")
+	logging.info("Done! Cleaning up files...")
 	for image in os.listdir("./cropped"):
 		os.remove("./cropped/" + image)
 
-	print("Done cleaning up files! It was fun creating a /r/Place with you!")
+	logging.info("Done cleaning up files! It was fun creating a /r/Place with you!")
 
 
 if __name__ == "__main__":
